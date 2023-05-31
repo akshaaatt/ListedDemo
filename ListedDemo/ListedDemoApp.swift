43e34beffbca1a723f0c7b74599741ec22067e20
Akshat Tiwari <51470769+akshaaatt@users.noreply.github.com>
2023-05-31 20:52:17 +0530

    Initial Commit

---
+++ ListedDemo/ListedDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ListedDemoApp.swift
+//  ListedDemo
+//
+//  Created by avataar on 31/05/23.
+//
+
+import SwiftUI
+
+@main
+struct ListedDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
